55341fdefdb8448d38ca4822c2e195d7602d36fe
parthp3 <parthp@krishworks.com>
2024-04-04 13:28:51 +0530

    lock

--- pages/api/hook.ts
+++ pages/api/hook.ts
@@ -4,21 +4,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const smartapp = new SmartApp()
     .enableEventLogging(2) // logs all lifecycle event requests and responses as pretty-printed JSON. Omit in production
     .page('mainPage', (context, page, configData) => {
-      page.section('sensors', section => {
-          section
-              .deviceSetting('contactSensor')
-              .capabilities(['contactSensor'])
-      });
-      page.section('lights', section => {
-          section
-              .deviceSetting('lights')
-              .capabilities(['switch'])
-              .permissions('rx')
-              .multiple(true);
-      });
       page.section('lock', section => {
         section
-            .deviceSetting('locl')
+            .deviceSetting('lock')
             .capabilities(['lock'])
             .permissions('rx')
             .multiple(true);
@@ -27,11 +15,10 @@ const smartapp = new SmartApp()
   // Called for both INSTALLED and UPDATED lifecycle events if there is no separate installed() handler
   .updated(async (context, updateData) => {
       await context.api.subscriptions.delete() // clear any existing configuration
-      await context.api.subscriptions.subscribeToDevices(context.config.contactSensor, 'contactSensor', 'contact', 'myDeviceEventHandler');
+      await context.api.subscriptions.subscribeToDevices(context.config.lock, 'lock', 'lock', 'myDeviceEventHandler');
   })
   .subscribedEventHandler('myDeviceEventHandler', async (context, event) => {
-      const value = event.value === 'open' ? 'on' : 'off';
-      await context.api.devices.sendCommands(context.config.lights, 'switch', value);
+    console.log(event)
   });
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
